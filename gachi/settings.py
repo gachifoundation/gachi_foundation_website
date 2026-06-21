12a6fc2224955448c966139f0ed7c510a24ee306
James Julius Tudu <jjtudu@gmail.com>
2026-06-21 18:11:29 +0530

    Fix sqlite ENGINE path

--- gachi/settings.py
+++ gachi/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = "gachi.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.sqlite3",
+        "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
